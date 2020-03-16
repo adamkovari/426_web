82076c4c078f8f3d26731c8a13813fcdbd7ac28d
zsoltadam <zsoltadam@hotmail.com>
2020-03-16 13:09:30 +0100

    site_build_1.2

---
+++ js/js.js
@@ -0,0 +1,38 @@
+function startShowing_nav(){
+    var elementToHide = document.getElementById("nav");
+    elementToHide.style.opacity = 0;
+    var intervalId = setInterval(function(){
+        if(elementToHide.style.opacity >= 1)
+        {
+            clearInterval(intervalId);
+        }else{
+            elementToHide.style.opacity = parseFloat(elementToHide.style.opacity) + 0.05;
+        }
+    },40);
+}
+
+function startShowing_nav_background(){
+    var elementToHide = document.getElementById("Réteg_1");
+    elementToHide.style.opacity = 0;
+    var intervalId = setInterval(function(){
+        if(elementToHide.style.opacity >= 0.3)
+        {
+            clearInterval(intervalId);
+        }else{
+            elementToHide.style.opacity = parseFloat(elementToHide.style.opacity) + 0.01;
+        }
+    },40);
+}
+
+function startShowing_bipi(){
+    var elementToHide = document.getElementById("bipi");
+    elementToHide.style.opacity = 0;
+    var intervalId = setInterval(function(){
+        if(elementToHide.style.opacity >= 0.5)
+        {
+            clearInterval(intervalId);
+        }else{
+            elementToHide.style.opacity = parseFloat(elementToHide.style.opacity) + 0.005;
+        }
+    },25);
+}
